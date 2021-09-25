5acc004161a7c2a633ca6e04bbad5c2db7e8a505
Ivan Sys <ilsimim@yandex.ru>
2021-09-25 22:06:55 +0300

    Row creation in DB added when a row with a given PK exists in Excel, but not in DB

--- upload.py
+++ upload.py
@@ -7,6 +7,7 @@ password = '1234'
 host = 'localhost'
 
 
+# Функция создания временного слованя с данными из файла Excel
 def create_temp_dict(data_frame):
     res_dict = {}
 
@@ -16,19 +17,29 @@ def create_temp_dict(data_frame):
     return res_dict
 
 
+# Функция проверки и обновления данных
 def update_db(query_temp_dict, dataframe):
     temp_dict = create_temp_dict(dataframe)
 
+    print(temp_dict)
+    print(query_temp_dict)
+
     for el in temp_dict:
-        if query_temp_dict[el] != temp_dict[el]:
+        if not query_temp_dict.get(el):
+            cursor.execute(""" INSERT INTO endpoint (endpoint_id, endpoint_name)
+                               VALUES (%s, %s) """, (str(el), str(temp_dict[el])))
+        elif query_temp_dict[el] != temp_dict[el]:
             cursor.execute(""" UPDATE endpoint
                                SET endpoint_name = (%s)
                                WHERE endpoint_id = (%s) """, (str(temp_dict[el]), str(el)))
+
     connection.commit()
 
     cursor.execute(""" SELECT * FROM endpoint """)
     return cursor.fetchall()
 
+
+# Устанавливаем соединение и создаём таблицу, если она не была создана
 
 connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 
@@ -40,17 +51,14 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS endpoint
 
 connection.commit()
 
+# Делаем запрос на БД и получаем текущие данные по endpoint_id/endpoint_name
+
 cursor.execute(""" SELECT * FROM endpoint """)
-try:
-    db_query_dict = dict(cursor.fetchall())
+db_query_dict = dict(cursor.fetchall())
 
-    endpoint_table = pd.read_excel('endpoint_names.xlsm')
+# Обновляем данные в БД
 
-    print(update_db(db_query_dict, endpoint_table))
+endpoint_table = pd.read_excel(r'c:\\upload_names\\endpoint_names.xlsm')
+print(update_db(db_query_dict, endpoint_table))
 
-    print('Обновление данных завершено')
-
-    exit_input = input('Вы хотите выйти? ')
-
-except:
-    print("Лалала")
+print('Обновление данных завершено')
